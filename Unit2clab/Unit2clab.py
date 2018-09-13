891fc59f53dad94cde52a4510225b27b54f197e3
firerex145 <firerex145@gmail.com>
2018-09-13 09:16:04 -0700

    hi

---
+++ Unit2clab/Unit2clab.py
@@ -0,0 +1,16 @@
+numList = [1,2,3,4,5,6,7,8,9,10]
+print(numList)
+print( len (numList))
+subList = numList[0]
+print(subList)
+subList2 = numList[9]
+print(subList2)
+myClasses = ['mandarin','intro to calc','independant study','economics','python programming','AP comp','prayer','US government','marine biology']
+myClasses.remove('intro to calc')
+print(myClasses)
+myClasses.pop()
+
+
+
+
+
